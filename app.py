81087ef48984897de73d407f1684400c58dd5949
Visakh Vijay O <92223768+vskvj3@users.noreply.github.com>
2024-06-25 13:27:11 +0530

    fix: linting issues in document module

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, send_file
+'''
+Flask Application
+'''
+
 import os
-from document import DocumentManager
 import datetime
+from flask import Flask, render_template, request, redirect, url_for, send_file
+from document import DocumentManager
 
 app = Flask(__name__)
 
@@ -13,6 +17,12 @@ document_manager = DocumentManager()
 
 @app.route('/')
 def index():
+    """
+    Renders the index page with a list of all documents.
+
+    Returns:
+        The rendered index.html template with the documents.
+    """
     document_files = document_manager.get_all_documents()
     print(document_files)
 
@@ -21,6 +31,14 @@ def index():
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
+    """
+    Handles the upload of a new document.
+
+    Returns:
+        If the request method is GET, renders the upload.html template.
+        If the request method is POST, stores the file and stored the data in db.
+        If the request method is POST and no file is selected, prints an error message.
+    """
     if request.method == 'POST':
         title = request.form['title']
         description = request.form['description']
@@ -29,31 +47,48 @@ def upload():
         if file:
             file_path = os.path.join(
                 app.config['UPLOAD_FOLDER'], file.filename)
-            
+
             # add time with file name to avoid copy
             file_name, file_extension = os.path.splitext(file_path)
             timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
             new_file_path = f"{file_name}_{timestamp}{file_extension}"
             file_path = new_file_path
-            
+
             file.save(file_path)
-                
+
             document_manager.add_document(title, description, file_path)
             print('Document uploaded successfully!')
-            return redirect(url_for('index'))
-        else:
-            print('No file selected!')
+        return redirect(url_for('index'))
     return render_template('upload.html')
 
 
 @app.route('/document/<int:id>')
 def view_document(id):
+    """
+    Renders the view page for a specific document.
+
+    Args:
+        id (int): The ID of the document to view.
+
+    Returns:
+        The rendered view.html template with the document.
+    """
     document_file = document_manager.get_document(id)
     return render_template('view.html', document=document_file)
 
 
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update_document(id):
+    """
+    Handles the update of a document.
+
+    Args:
+        id (int): The ID of the document to update.
+
+    Returns:
+        If the request method is GET, renders the update.html template.
+        If the request method is POST, updates the document with given data.
+    """
     document = document_manager.get_document(id)
     print(document)
     if request.method == 'POST':
@@ -66,7 +101,7 @@ def update_document(id):
                 app.config['UPLOAD_FOLDER'], file.filename)
             file.save(file_path)
             new_file_path = file_path
-        
+
         update_dict = {}
         if title:
             update_dict['title'] = title
@@ -76,7 +111,7 @@ def update_document(id):
             update_dict['file_path'] = new_file_path
 
         document_manager.update_document(id, update_dict)
-        
+
         print('Document updated successfully!')
 
         return redirect(url_for('index'))
@@ -86,12 +121,30 @@ def update_document(id):
 
 @app.route('/download/<int:id>')
 def download_document(id):
+    """
+    Downloads a specific document.
+
+    Args:
+        id (int): The ID of the document to download.
+
+    Returns:
+        The document file as an attachment.
+    """
     document_file = document_manager.get_document(id)
     return send_file(document_file["file_path"], as_attachment=True)
 
 
 @app.route('/delete/<int:id>', methods=['POST'])
 def delete_document(id):
+    """
+    Deletes a specific document.
+
+    Args:
+        id (int): The ID of the document to delete.
+
+    Returns:
+        Redirects to the index page after deleting the document.
+    """
     try:
         document_file = document_manager.get_document(id)
         os.remove(document_file['file_path'])
--- document.py
+++ document.py
@@ -1,9 +1,26 @@
+'''
+DocumentManger class to handle the CRUD operations on the documents table in the SQLite database.
+'''
 import sqlite3
-import os
-import datetime
 
 
 class DocumentManager:
+    """
+    A class for managing documents in a SQLite database.
+
+    Attributes:
+        connection (sqlite3.Connection): The connection to the SQLite database.
+
+    Methods:
+        __init__(self, db_name='data/files.sqlite'): Initializes a DocumentManager object.
+        create_table(self): Creates the 'documents' table if it doesn't exist in the database.
+        add_document(self, title, description, file_path): Adds a new document to documents table.
+        delete_document(self, doc_id): Removes an entry from the documents table using doc_id.
+        update_document(self, doc_id, update_dict): Updates a document in the documents table.
+        get_document(self, doc_id): Retrieves a document from the documents table.
+        get_all_documents(self): Retrieves all documents from the documents table.
+    """
+
     def __init__(self, db_name='data/files.sqlite'):
         """
         Initializes a DocumentManager object.
@@ -20,6 +37,7 @@ class DocumentManager:
             self.create_table()
         except sqlite3.Error as e:
             raise sqlite3.Error("Could not connect database") from e
+
         except AttributeError as e:
             raise AttributeError("Could not create table") from e
 
@@ -35,7 +53,7 @@ class DocumentManager:
                 self.connection.execute(
                     '''
                     CREATE TABLE IF NOT EXISTS documents (
-                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        doc_id INTEGER PRIMARY KEY AUTOINCREMENT,
                         title TEXT,
                         description TEXT,
                         file_path TEXT
@@ -56,7 +74,6 @@ class DocumentManager:
             sqlite3.Error: If there is an error adding the document to the table.
         """
         try:
-            # 
             with self.connection:
                 self.connection.execute(
                     '''
@@ -66,12 +83,12 @@ class DocumentManager:
         except sqlite3.Error as e:
             raise sqlite3.Error("Could not add document", e) from e
 
-    def delete_document(self, id):
+    def delete_document(self, doc_id):
         """
-        Removes an entry from the 'documents' table using the specified id.
+        Removes an entry from the 'documents' table using the specified doc_id.
 
         Parameters:
-            id (int): The id of the document to be deleted.
+            doc_id (int): The doc_id of the document to be deleted.
 
         Raises:
             sqlite3.Error: If there is an error deleting the document from the table.
@@ -80,17 +97,17 @@ class DocumentManager:
             with self.connection:
                 self.connection.execute(
                     '''
-                    DELETE FROM documents WHERE id=?
-                    ''', (id,))
+                    DELETE FROM documents WHERE doc_id=?
+                    ''', (doc_id,))
         except sqlite3.Error as e:
             raise sqlite3.Error("Could not delete document") from e
-        
-    def update_document(self, id, update_dict):
+
+    def update_document(self, doc_id, update_dict):
         """
-         Updates a document in the 'documents' table using the specified id and update dictionary.
+         Updates a document in the documents table using the specified doc_id and update dictionary.
 
         Parameters:
-            id (int): The id of the document to be updated.
+            doc_id (int): The doc_id of the document to be updated.
             update_dict (dict): A dictionary containing the fields to be updated.
 
         Raises:
@@ -104,18 +121,18 @@ class DocumentManager:
                     update_query += f"{key}=?, "
                     update_values.append(value)
                 update_query = update_query.rstrip(", ")
-                update_query += " WHERE id=?"
-                update_values.append(id)
+                update_query += " WHERE doc_id=?"
+                update_values.append(doc_id)
                 self.connection.execute(update_query, update_values)
         except sqlite3.Error as e:
             raise sqlite3.Error("Could not update document") from e
 
-    def get_document(self, id):
+    def get_document(self, doc_id):
         """
-        Retrieves a document from the 'documents' table using the specified id.
+        Retrieves a document from the 'documents' table using the specified doc_id.
 
         Parameters:
-            id (int): The id of the document to be retrieved.
+            doc_id (int): The doc_id of the document to be retrieved.
 
         Returns:
             dict: A dictionary containing the document information.
@@ -127,11 +144,11 @@ class DocumentManager:
             with self.connection:
                 cursor = self.connection.execute(
                     '''
-                    SELECT * FROM documents WHERE id=?
-                    ''', (id,))
+                    SELECT * FROM documents WHERE doc_id=?
+                    ''', (doc_id,))
                 document = cursor.fetchone()
                 document_dict = {
-                    'id': document[0],
+                    'doc_id': document[0],
                     'title': document[1],
                     'description': document[2],
                     'file_path': document[3]
@@ -160,7 +177,7 @@ class DocumentManager:
             documents_list = []
             for document in document_files:
                 documents_list.append({
-                    'id': document[0],
+                    'doc_id': document[0],
                     'title': document[1],
                     'description': document[2],
                     'file_path': document[3]
